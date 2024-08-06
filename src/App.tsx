b17be6de86379a0b8fb12b1def572d67674ff799
Qheuss <36819862+Qheuss@users.noreply.github.com>
2024-08-06 21:50:07 +0200

    [UPDATE] inventory route as / and not /inventory

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
-import Home from './pages/Home/Page';
+// import Home from './pages/Home/Page';
 import Inventory from './pages/Inventory/Page';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='*' element={<Home />} />
-        <Route path='/inventory' element={<Inventory />} />
+        {/* <Route path='/' element={<Home />} /> */}
+        {/* <Route path='*' element={<Home />} /> */}
+        <Route path='*' element={<Inventory />} />
+        <Route path='/' element={<Inventory />} />
         {/* <Route path='/game/:gameId' element={<GamePage />} /> */}
       </Routes>
     </BrowserRouter>
